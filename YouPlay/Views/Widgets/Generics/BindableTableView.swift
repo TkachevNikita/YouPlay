8758d2e7817c8a03168407ba0b735c38b83bd3ca
Tkachev.Nikita <n.o.tkachev@urfu.ru>
2024-12-17 15:01:55 +0500

    feat: add founder page view

---
+++ YouPlay/Views/Widgets/Generics/BindableTableView.swift
@@ -0,0 +1,98 @@
+import Foundation
+import RxSwift
+import RxDataSources
+import SnapKit
+import RxRelay
+import RxCocoa
+
+class BindableTableViewController<T: AnimatableSectionModelType>: UIViewController, UITableViewDelegate, UITableViewDragDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let dragItem = UIDragItem(itemProvider: NSItemProvider())
+        dragItem.localObject = tableView.cellForRow(at: indexPath)
+        return [dragItem]
+    }
+    
+    var items: RxSwift.Observable<[T]>?
+    
+    var onItemSelected: ((IndexPath) -> Void)?
+    
+    var onItemMoved: ((ItemMovedEvent) -> Void)?
+    
+    var onItemRemoved: ((IndexPath) -> Void)?
+    
+    var tableView: UITableView
+    
+    var classesToRegister: [String: AnyClass]
+    
+    weak var dataSource: RxTableViewSectionedAnimatedDataSource<T>?
+    
+    var heightForRow: CGFloat
+    
+    var supportsDragging: Bool
+    
+    private let disposeBag = DisposeBag()
+    
+    init(items: RxSwift.Observable<[T]>?,
+         heightForRow: CGFloat,
+         tableViewColor: UIColor = .clear,
+         onItemSelected: ((IndexPath) -> Void)? = nil,
+         onItemMoved: ((ItemMovedEvent) -> Void)? = nil,
+         onItemRemoved: ((IndexPath) -> Void)? = nil,
+         dataSource: RxTableViewSectionedAnimatedDataSource<T>,
+         classesToRegister: [String: AnyClass],
+         supportsDragging: Bool = false,
+         style: UITableView.Style? = .plain) {
+        self.heightForRow = heightForRow
+        self.items = items
+        self.dataSource = dataSource
+        self.classesToRegister = classesToRegister
+        self.onItemSelected = onItemSelected
+        self.onItemMoved = onItemMoved
+        self.onItemRemoved = onItemRemoved
+        self.supportsDragging = supportsDragging
+        tableView = UITableView(frame: .zero, style: style ?? .plain)
+        super.init(nibName: nil, bundle: nil)
+        tableView.backgroundColor = tableViewColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return heightForRow
+    }
+        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for (key, value) in classesToRegister {
+            tableView.register(value, forCellReuseIdentifier: key)
+        }
+        
+        items?.bind(to: tableView.rx.items(dataSource: dataSource!))
+            .disposed(by: disposeBag)
+        
+        tableView.rx.itemSelected
+            .asDriver()
+            .drive(onNext: onItemSelected)
+            .disposed(by: disposeBag)
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.size.equalToSuperview()
+        }
+        
+        tableView.separatorColor = .clear
+        
+        if supportsDragging {
+            tableView.dragDelegate = self
+            tableView.rx.itemMoved
+                .asDriver()
+                .drive(onNext: onItemMoved)
+                .disposed(by: disposeBag)
+        }
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+}
---
+++ YouPlay/Views/Widgets/ProgressCircleView.swift
@@ -0,0 +1,96 @@
+import Foundation
+import UIKit
+
+class ProgressCircleView: UIView {
+    
+    private let shapeLayer = CAShapeLayer()
+    
+    private let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+    
+    public var strokeColor: CGColor = .init(gray: 1, alpha: 1)
+    
+    public var strokeWidth: CGFloat = 10
+    
+    public var currentProgress: CGFloat {
+        didSet {
+            updateProgress()
+        }
+    }
+    
+    public var lastProgress: CGFloat = 0
+    
+    public var fillColor: CGColor
+    
+    public var updateTimeInterval: TimeInterval
+    
+    var progressLabel: UILabel!
+    
+    init(currentProgress: CGFloat = 0,
+         fillColor: CGColor = UIColor.clear.cgColor,
+         showProgressLabel: Bool = false,
+         updateTimeInterval: TimeInterval = 0.1) {
+        self.currentProgress = currentProgress
+        self.fillColor = fillColor
+        self.updateTimeInterval = updateTimeInterval
+        super.init(frame: .zero)
+        if showProgressLabel {
+            addView()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addView() {
+        
+        progressLabel = {
+            let label = UILabel()
+            label.textAlignment = .center
+            label.font = .boldSystemFont(ofSize: 60)
+            label.textColor = .white
+            label.adjustsFontSizeToFitWidth = true
+            return label
+        }()
+        
+        addSubview(progressLabel)
+        progressLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(strokeWidth)
+            make.right.equalToSuperview().inset(strokeWidth)
+            make.top.equalToSuperview().offset(strokeWidth)
+            make.bottom.equalToSuperview().inset(strokeWidth)
+        }
+    }
+    
+    @objc public func updateProgress() {
+        progressLabel?.text = "\(Int(currentProgress*100))%"
+        basicAnimation.fromValue = lastProgress
+        
+        if (lastProgress != 1) {
+            if (currentProgress != lastProgress) {
+                basicAnimation.isRemovedOnCompletion = false
+                basicAnimation.toValue = currentProgress
+                basicAnimation.fillMode = .forwards
+                lastProgress = currentProgress
+                shapeLayer.add(basicAnimation, forKey: "basicAnimation")
+            }
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let circularPath = UIBezierPath(arcCenter: CGPoint(x: bounds.midX, y: bounds.midY),
+                                        radius: frame.size.height / 2,
+                                        startAngle: -.pi / 2,
+                                        endAngle: 2 * .pi - .pi / 2,
+                                        clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.green.cgColor
+        shapeLayer.lineWidth = strokeWidth
+        shapeLayer.strokeEnd = 0
+        shapeLayer.fillColor = fillColor
+        
+        layer.addSublayer(shapeLayer)
+    }
+}
